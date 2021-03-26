384fb30c789c6bca3d78163718f4a302fafac642
haili.lhq <haili.lhq@alibaba-inc.com>
2021-03-26 17:40:04 +0800

    fix(cqa): update git repository

--- bin/utils/config.js
+++ bin/utils/config.js
@@ -2,11 +2,11 @@ const FRAMEWORK_ORIGIN = 'origin';
 const FRAMEWORK_REACT = 'react';
 
 const FRAMEWORK_BRANCH = {
-  [FRAMEWORK_ORIGIN]: 'demo',
-  [FRAMEWORK_REACT]: 'react-demo',
+  [FRAMEWORK_ORIGIN]: 'template/origin',
+  [FRAMEWORK_REACT]: 'template/react',
 };
 
-const GIT = 'https://github.com/wloscar/demo-project.git';
+const GIT = 'https://github.com/haili042/create-qbi-app.git';
 
 module.exports = {
   FRAMEWORK_ORIGIN,
