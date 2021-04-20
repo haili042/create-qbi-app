ae42fbdfa87f08467123c189fe962b22ba30cf04
haili.lhq <haili.lhq@alibaba-inc.com>
2021-04-20 11:18:23 +0800

    feat(cqa): support vue

--- bin/utils/config.js
+++ bin/utils/config.js
@@ -1,9 +1,11 @@
 const FRAMEWORK_ORIGIN = 'origin';
 const FRAMEWORK_REACT = 'react';
+const FRAMEWORK_VUE = 'vue';
 
 const FRAMEWORK_BRANCH = {
   [FRAMEWORK_ORIGIN]: 'template/origin',
   [FRAMEWORK_REACT]: 'template/react',
+  [FRAMEWORK_VUE]: 'template/vue',
 };
 
 const GIT = 'https://github.com/haili042/create-qbi-app.git';
@@ -12,5 +14,6 @@ module.exports = {
   FRAMEWORK_ORIGIN,
   FRAMEWORK_REACT,
   FRAMEWORK_BRANCH,
+  FRAMEWORK_VUE,
   GIT,
 };
